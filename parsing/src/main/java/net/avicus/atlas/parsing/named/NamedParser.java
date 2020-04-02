92ba9c5e2af12d8f36e8357db722b3f523fbd817
Austin Mayes <austin@avicus.net>
2020-04-02 16:58:37 -0500

    Start work on modern parsing system

---
+++ parsing/src/main/java/net/avicus/atlas/parsing/named/NamedParser.java
@@ -0,0 +1,21 @@
+package net.avicus.atlas.parsing.named;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Represents a method used to parse something referenced by string.
+ *
+ * @author Avicus Network
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NamedParser {
+
+  /**
+   * @return identification key representing what the linked method should parse
+   */
+  String[] value();
+}
