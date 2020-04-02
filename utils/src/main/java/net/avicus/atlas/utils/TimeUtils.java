0c25dfe0c15b1146e3bb25173f47cebcf6fed622
Austin Mayes <austin@avicus.net>
2020-04-02 16:56:08 -0500

    Time utils

--- utils/src/main/java/net/avicus/atlas/utils/TimeUtils.java
+++ utils/src/main/java/net/avicus/atlas/utils/TimeUtils.java
@@ -1,5 +1,8 @@
 package net.avicus.atlas.utils;
 
+import java.time.Duration;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.ocpsoft.prettytime.TimeUnit;
@@ -7,6 +10,8 @@ import org.ocpsoft.prettytime.impl.ResourcesTimeFormat;
 import org.ocpsoft.prettytime.units.JustNow;
 
 public final class TimeUtils {
+
+  public static final Duration INF_POSITIVE = ChronoUnit.FOREVER.getDuration();
 
   public static PrettyTime prettyTime(Locale locale) {
     final PrettyTime pretty = new PrettyTime(locale);
@@ -34,5 +39,66 @@ public final class TimeUtils {
   public static long[] secToMs(final double s1) {
     final long v = (long) (s1 * 1000d);
     return new long[]{v, v, v, v, v, v};
+  }
+
+
+  /**
+   * Parse a duration, and treat simple numbers with no declaration as pure seconds.
+   *
+   * @param text to parse
+   * @return the parsed duration
+   * @throws DateTimeParseException if parsing fails
+   */
+  public static Duration parseDurationOrSeconds(String text) throws DateTimeParseException {
+    if ("oo".equals(text)) {
+      return INF_POSITIVE;
+    }
+
+    // If text looks like a plain number, try to parse it as seconds,
+    // but be fairly strict so we don't accidentally parse a time as
+    // a number.
+    if (text.matches("^\\s*-?[0-9]+(\\.[0-9]+)?\\s*$")) {
+      try {
+        return Duration.ofMillis((long) (1000 * Double.parseDouble(text)));
+      } catch (NumberFormatException ignored) {
+      }
+    }
+
+    return parseDuration(text);
+  }
+
+  /**
+   * See {@link #parseDurationOrSeconds(String)}. If the string is empty, the default argument will
+   * be returned.
+   *
+   * @throws DateTimeParseException if parsing of the text fails
+   */
+  public static Duration parseDurationOrSeconds(String text, Duration def)
+      throws DateTimeParseException {
+    if (text == null || text.length() == 0) {
+      return def;
+    } else {
+      return parseDurationOrSeconds(text);
+    }
+  }
+
+  /**
+   * Parse a {@link Duration} from a formatted string, adding on the needed prefix as needed.
+   *
+   * @param text to parse
+   * @return the duration represented by the string
+   * @throws DateTimeParseException if parsing fails
+   */
+  public static Duration parseDuration(CharSequence text) throws DateTimeParseException {
+    if (text.length() > 0) {
+      switch (text.charAt(0)) {
+        case '-':
+        case 'P':
+        case 'p':
+          return Duration.parse(text);
+      }
+    }
+
+    return Duration.parse("PT" + text);
   }
 }
