c2fbb5a1d20148ab7e2040a8d6ade2704284a904
Austin Mayes <austin@avicus.net>
2020-04-02 16:57:21 -0500

    Start work on document bridge

---
+++ document-bridge/src/main/java/net/avicus/atlas/document/StringHolder.java
@@ -0,0 +1,33 @@
+package net.avicus.atlas.document;
+
+import java.util.Optional;
+
+/**
+ * Something that has the potential to hold a string value.
+ *
+ * @author Austin Mayes
+ */
+public interface StringHolder {
+
+  /**
+   * Check if this representation contains any data.
+   */
+  boolean isValuePresent();
+
+  /**
+   * Convenience method for {@link #value()} that will throw an error if no value is present.
+   *
+   * @throws ParsingException jf no text is present
+   */
+  String asRequiredString() throws ParsingException;
+
+  /**
+   * The value of this attribute, if it exists.
+   */
+  Optional<String> value();
+
+  /**
+   * The {@link Node} which this holder is inside of. This is never null.
+   */
+  Node parent();
+}
